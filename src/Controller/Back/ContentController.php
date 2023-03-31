b0b65787464ebeac9847faac254afef08ac0b8a2
ramis <lucas.ramis7@gmail.com>
2023-03-31 22:11:49 +0200

    :sparkles: create media & content_text table / Migration

--- src/Controller/Back/ContentController.php
+++ src/Controller/Back/ContentController.php
@@ -6,6 +6,7 @@ use App\Entity\Content;
 use App\Entity\ContentText;
 use App\Entity\Media;
 use App\Form\ContentFormType;
+use App\Repository\ContentRepository;
 use App\Repository\ContentTypeRepository;
 use App\Repository\MediaRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -20,16 +21,19 @@ class ContentController extends AbstractController
     private EntityManagerInterface $em;
     private MediaRepository $mediaRepository;
     private ContentTypeRepository $contentTypeRepository;
+    private ContentRepository $contentRepository;
 
     public function __construct(
         EntityManagerInterface $em,
         MediaRepository $mediaRepository,
-        ContentTypeRepository $contentTypeRepository
+        ContentTypeRepository $contentTypeRepository,
+        ContentRepository $contentRepository
     )
     {
         $this->em = $em;
         $this->mediaRepository = $mediaRepository;
         $this->contentTypeRepository = $contentTypeRepository;
+        $this->contentRepository = $contentRepository;
     }
 
     #[Route('/create', name: 'create')]
@@ -70,6 +74,60 @@ class ContentController extends AbstractController
         return $this->render('back/default/create.html.twig', [
             'form' => $form->createView(),
             'formData' => $content
+        ]);
+    }
+
+    #[Route('/list', name: 'list')]
+    public function listContents(ContentRepository $contentRepository): Response
+    {
+        $contents = $contentRepository->findAll();
+
+        return $this->render('back/default/list.html.twig', [
+            'controller_name' => 'GuideController',
+            'contents' => $contents,
+        ]);
+    }
+    #[Route('/show/{slug}', name: 'show')]
+    public function showContent(Content $content, Request $request): Response
+    {
+        //$content = $this->contentRepository->findOneBy(['slug' => $slug]);
+        $form = $this->createForm(ContentFormType::class, $content);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid() && $request->isMethod('post')){
+            $file = $form->get('file_header')->getData();
+            $filename =  '/uploads/media/' . substr(md5(uniqid()), 0, 8).'.'.$file->guessExtension();
+            $file->move(
+                'uploads/media/',
+                $filename
+            );
+
+            $contentType =  $this->contentTypeRepository->find(1);
+            $dscp = $content->getDescription();
+            $content->setDescription(substr($dscp, 0, 50));
+            $content->setContentType($contentType);
+            $content->addTag($form->get('selectTag')->getData());
+            $this->em->persist($content);
+            $contentText = (new ContentText())
+                ->setContent($content)
+                ->setText($dscp);
+            $this->em->persist($contentText);
+
+            $media = (new Media())
+                ->setContent($content)
+                //->setSlug(strtolower(basename($file, $file->guessExtension())))
+                ->setType('img')
+                ->setFilename($filename);
+            $this->em->persist($media);
+            $this->em->flush();
+
+            return $this->redirectToRoute("back_default_show");
+        }
+
+        return $this->render('back/default/show.html.twig', [
+            'controller_name' => 'GuideController',
+            'content' => $content,
+            'form' => $form->createView(),
+            'formData' => $content,
         ]);
     }
 }
